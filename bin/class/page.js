378f48ad636a01681285baf35010be49b2006129
Pedro Vinícius <62823394+pedrovinis@users.noreply.github.com>
2021-08-18 17:42:02 -0300

    pupteerTimeOut bug fixed

--- bin/class/page.js
+++ bin/class/page.js
@@ -11,6 +11,7 @@ class Page {
                 height: this.options.height
             })
             await this.page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36')
+            await this.page.setDefaultNavigationTimeout(0)
             return this
         })()
     }
